9f67c1ef99d343894ae8fcb23528f149ff6b0285
YusufIsmail <yusufgamer222@gmail.com>
2021-09-13 16:28:44 +0100

    Finished Login page. Register page next

--- src/net/yusuf/LoginPage.java
+++ src/net/yusuf/LoginPage.java
@@ -38,16 +38,19 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.HashMap;
 
+import static java.awt.Font.ITALIC;
+
 public class LoginPage implements ActionListener {
 
     JFrame frame = new JFrame();
     JButton loginButton = new JButton("Login");
     JButton resetButton = new JButton("Reset");
+    JButton registerButton = new JButton("Register");
     JTextField userNameField = new JTextField();
     JPasswordField userPasswordField = new JPasswordField();
     JLabel userNameLabel = new JLabel("User name");
     JLabel userPasswordLabel = new JLabel("Password");
-    JLabel errorMessageLabel = new JLabel("Incorrect username or password.");
+    JLabel MessageLabel = new JLabel();
     HashMap<String, String> loginInfo;
 
     LoginPage(HashMap<String, String> loginInfoOriginal) {
@@ -57,11 +60,33 @@ public class LoginPage implements ActionListener {
         userNameLabel.setBounds(50,100,75,25);
         userPasswordLabel.setBounds(50,150,75,25);
 
-        errorMessageLabel.setBounds(125,250,250,35);
-        errorMessageLabel.setFont(new Font(null, Font.HANGING_BASELINE, 25));
+        MessageLabel.setBounds(125,300,250,35);
+        MessageLabel.setFont(new Font(null, ITALIC, 20));
+
+        userNameField.setBounds(125,100,200,25);
+        userPasswordField.setBounds(125,150,200,25);
+
+        registerButton.setBounds(175,240,100,25);
+        registerButton.setFocusable(false);
+        registerButton.addActionListener(this);
+
+        loginButton.setBounds(225,200,100,25);
+        loginButton.setFocusable(false);
+        loginButton.addActionListener(this);
+
+        resetButton.setBounds(125,200,100,25);
+        resetButton.setFocusable(false);
+        resetButton.addActionListener(this);
+
 
         frame.add(userNameLabel);
         frame.add(userPasswordLabel);
+        frame.add(MessageLabel);
+        frame.add(userNameField);
+        frame.add(userPasswordField);
+        frame.add(loginButton);
+        frame.add(resetButton);
+        frame.add(registerButton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLayout(null);
@@ -71,5 +96,35 @@ public class LoginPage implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(e.getSource() == resetButton) {
+            userNameField.setText("");
+            userPasswordField.setText("");
+        }
+
+        if(e.getSource() == loginButton) {
+
+            String userName = userNameField.getText();
+            String password = String.valueOf(userPasswordField.getPassword());
+
+
+            if(loginInfo.containsKey(userName)) {
+                if(loginInfo.get(userName).equals(password)) {
+                    MessageLabel.setForeground(Color.CYAN);
+                    MessageLabel.setText("Login successful");
+                    frame.dispose();
+                    WelcomePage welcomePage = new WelcomePage(userName);
+                } else {
+                    MessageLabel.setForeground(Color.RED);
+                    MessageLabel.setText("password is incorrect");
+                }
+            }
+            else {
+                MessageLabel.setForeground(Color.RED);
+                MessageLabel.setText("User name is incorrect");
+            }
+        }
+        if(e.getSource() == resetButton) {
+            RegisterPage registerPage = new RegisterPage();
+        }
     }
 }
--- src/net/yusuf/WelcomePage.java
+++ src/net/yusuf/WelcomePage.java
@@ -33,5 +33,24 @@
 
 package net.yusuf;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class WelcomePage {
+
+    JFrame frame = new JFrame();
+    JLabel welcomeLabel = new JLabel("Hi!");
+
+    WelcomePage(String userName) {
+
+        welcomeLabel.setBounds(0,0,200,35);
+        welcomeLabel.setFont(new Font(null, Font.BOLD, 25));
+        welcomeLabel.setText("Hi " + userName);
+
+        frame.add(welcomeLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
